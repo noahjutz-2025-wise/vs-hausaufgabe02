d6b68f3a0691c4e86e562a0c5812d18cd7f45f38
Noah Jutz <noah@noahjutz.com>
2025-11-18 22:29:18 +0100

    Implement Consumer (client)

---
+++ src/main/java/com/noahjutz/Consumer.java
@@ -0,0 +1,17 @@
+package com.noahjutz;
+
+import com.noahjutz.proto.HelloServiceGrpc;
+import com.noahjutz.proto.HelloWorld;
+import io.grpc.ManagedChannelBuilder;
+
+public class Consumer {
+  static void main() {
+    var channel = ManagedChannelBuilder.forAddress("localhost", 3000)
+      .usePlaintext()
+      .build();
+
+    var stub = HelloServiceGrpc.newBlockingStub(channel);
+
+    stub.sayHello(HelloWorld.newBuilder().setHello("Hiiii :3").build());
+  }
+}
--- src/main/java/com/noahjutz/Main.java
+++
@@ -1,7 +0,0 @@
-package com.noahjutz;
-
-public class Main {
-  static void main() {
-    IO.println("Hello and welcome!");
-  }
-}
