565fd50acd331930fa1170818549576286586d25
Noah Jutz <noah@noahjutz.com>
2025-11-19 13:32:04 +0100

    Implement Producer (server)

---
+++ src/main/java/com/noahjutz/Producer.java
@@ -0,0 +1,33 @@
+package com.noahjutz;
+
+import com.google.protobuf.Empty;
+import com.noahjutz.proto.HelloServiceGrpc;
+import com.noahjutz.proto.HelloWorld;
+import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+class Service extends HelloServiceGrpc.HelloServiceImplBase {
+  @Override
+  public void sayHello(HelloWorld request, StreamObserver<Empty> responseObserver) {
+    System.out.println(request);
+    responseObserver.onNext(Empty.newBuilder().build());
+    responseObserver.onCompleted();
+  }
+}
+
+public class Producer {
+  static void main() throws IOException, InterruptedException {
+    var server = ServerBuilder.forPort(3000)
+      .addService(ProtoReflectionService.newInstance())
+      .addService(new Service())
+      .build();
+
+    server.start();
+    Runtime.getRuntime().addShutdownHook(new Thread(server::shutdownNow));
+
+    server.awaitTermination();
+  }
+}
